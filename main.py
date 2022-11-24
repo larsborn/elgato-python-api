2b55470671d2c0cf6f9cd6f092f7154a62ff58b0
Christopher Voss <voss@ins.uni-bonn.de>
2022-11-24 16:16:05 +0100

    it could work now

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import random
 import yaml, sys
 from typing import Dict
+import colour
 
 import requests.adapters
 
@@ -66,26 +67,64 @@ class Endpoints:
         }
         """
 
+class ColorRotator:
+    def __init__(self, colors_from_config: dict, mode: dict):
+        self._step = 0
+        self._gradient_steps = 10
+        # prepare our color dictionary from config
+        self._colors = {}
+        for k, v in colors_from_config.items():
+            r, g, b = map(int, v.split(','))
+            self._colors[k] = colour.Color(rgb=(r/255,g/255,b/255))
+
+        # create the gradients:
+        gradient_color_generators = []
+        for i in range(len(mode['colors']) - 1):
+            c1 = self._colors[mode['colors'][i]]
+            c2 = self._colors[mode['colors'][i + 1]]
+            gradient_color_generators.append(c1.range_to(c2, self._gradient_steps))
+        self._color_rotation = []
+        for range_generator in gradient_color_generators:
+            for color in range_generator:
+                self._color_rotation.append(color)
+
+    def get_next_color(self):
+        color = self._color_rotation[self._step]
+        self._step + 1
+        if self._step == len(self._color_rotation):
+            self._step = 0
+        return color
+
 class ElgatoConfig:
     def __init__(self, yml_config: dict, mode: str):
         self._ip = yml_config['ip']
         self.base_url = f"http://{self._ip}:9123"
         self.verbose = yml_config['verbose']
-
-        self._colors = yml_config['colors']
         self._mode = yml_config['modes'][mode]
-        self._hue_change_type = self.mode['type']
+        self._hue_change_type = self._mode['type']
 
-    def get_next_hue(self, hue: int):
         if self._hue_change_type == 'random':
-            return random.randint(0, 359)
+            self.get_next_hue = self.get_next_hue_random
         if self._hue_change_type == 'rotate':
-            return (hue + 1) % 360
+            self.get_next_hue = self.get_next_hue_rotate
         if self._hue_change_type == 'linear':
-            # TODO
-            return hue
-        print(f"hue changing type {self._hue_change_type} not recognized, not changing hue.")
-        return hue
+            if len(self._mode['colors']) < 2:
+                print(f"mode {self._mode} has less than 2 colors. Please specify at least two. Exiting")
+                sys.exit(3)
+            self._colorRotator = ColorRotator(
+                yml_config['colors'],
+                self._mode)
+            self.get_next_hue = self.get_next_hue_linear
+
+    def get_next_hue_random(self):
+        return random.randint(0, 359)
+
+    def get_next_hue_rotate(self, hue: int):
+        return (hue + 1) % 360
+
+    def get_next_hue_linear(self):
+        color = self._colorRotator.get_next_color()
+        return color.hue * 360
 
 class ElgatoApi:
     def __init__(self, config: ElgatoConfig):
@@ -112,25 +151,37 @@ class ElgatoApi:
             response = self._session.put(self._endpoints.lights, json={'lights': [data]})
             response.raise_for_status()
 
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default='config.yml')
     parser.add_argument('--mode', default='rotate')
     args = parser.parse_args()
 
+    # check if config file exists
     if not os.path.exists(args.config):
         print(f"could not find config file {args.config}. Exiting")
         sys.exit(1)
 
+    # read config file
     with open(args.config, "r") as f:
         yml_config = yaml.safe_load(f)
-        conf = ElgatoConfig(yml_config, args.mode)
 
+    # check if specified mode is defined in config
     if args.mode not in yml_config['modes'].keys():
         print(f"mode {args.mode} not specified in config. Exiting")
         sys.exit(2)
 
+    # for every color in every mode, check if we have a definition for it
+    color_lists = [v['colors'] for _, v in yml_config['modes'].items() if 'colors' in v]
+    # flatten
+    all_colors = [i for sublist in color_lists for i in sublist]
+    for color in all_colors:
+        if color not in yml_config['colors'].keys():
+            print(f"did not recognize color {color}, please define it in the config. Exiting")
+            sys.exit(3)
+    
+    # create config obj and api, run it
+    conf = ElgatoConfig(yml_config, args.mode)
     api = ElgatoApi(conf)
     api.run()
 
